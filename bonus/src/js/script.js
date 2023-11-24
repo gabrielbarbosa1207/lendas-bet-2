da797df7d01e7b48b4c8d55c94a1aedf1a372fcc
gabrielbarbosaweb <gabrielbarbosasa2@gmail.com>
2023-11-24 17:39:19 -0300

    new link

--- bonus/src/js/script.js
+++
@@ -1,8 +0,0 @@
-var back_redirect_back_link = 'https://tracker.lendasbet.com/link?btag=51375640_295513';
-history['pushState']({}, '', location['href']);
-history['pushState']({}, '', location['href']);
-window['onpopstate'] = function() {
-setTimeout(function() {
-	location['href'] = back_redirect_back_link
-	}, 1)
-}
